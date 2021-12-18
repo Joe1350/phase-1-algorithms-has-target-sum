04ddb8240eeb493ee9b85295ea6a1ff8cda74840
Joseph Martin <josephmartin1350@gmail.com>
2021-12-18 00:51:44 -0700

    commit tree 2ea3dcbfb50297ac5219e7ea0c4540dbbf3694c2

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {}
+  for (let number of array) {
+    const complement = target - number
+    if (complement in seenNumbers) {
+      return true
+    }
+    seenNumbers[number] = true
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+  O(n)
 */
 
 /* 
   Add your pseudocode here
+
+  function(array, target) {
+    create empty Object to place seenNumbers
+    for number of array
+      define the complementary number
+      if complement is seen in seen numbers
+        return true
+      not sure what this line is doing
+    if nothing returned true, return false
+  }
 */
 
 /*
   Add written explanation of your solution here
+
+  create a placeholder to store seen numbers so that runtime is O(n) instead of O(n ** 2)
+  itterate over the array
+  check that the current number's complement doesn't equal any of the seen numbers
+  if so, return true
+  if not, return false
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -32,3 +59,15 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
+
+// function hasTargetSum(array, target) {
+//   // Write your algorithm here
+//   for (let number of array) {
+//     for (i = 0; i < array.length - 1; i++) {
+//       if (array[i] + number === target) {
+//         return true;
+//       } 
+//     }
+//   }
+//   return false
+// }
